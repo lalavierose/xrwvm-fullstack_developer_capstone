bcce40150abddbf70f083df06d1f8fa6f8973543
lalavierose <halazidan02@gmail.com>
2024-08-09 10:11:12 -0400

    adding csrf extempt to views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -106,20 +106,24 @@ def get_dealer_details(request, dealer_id):
 
 
 # Create an `add_review` view to submit a review
+@csrf_exempt
 def add_review(request):
-    if (request.user.is_anonymous is False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
-        except:
-            return JsonResponse({"status": 401,
-                                 "message": "Error in posting review"})
+        except Exception:
+            return JsonResponse({
+                "status": 401,
+                "message": "Error in posting review"
+            })
         finally:
             print("add_review request successful!")
-    else:
-        return JsonResponse({"status": 403,
-                             "message": "Unauthorized"})
+    return JsonResponse({
+        "status": 403,
+        "message": "Unauthorized"
+    })
 
 
 # Method to get cars
